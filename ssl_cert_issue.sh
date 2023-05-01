6a211106f607f8e87b687a80500e9e71e9a5633e
Hawax <99415145+hawaxgit@users.noreply.github.com>
2023-05-01 22:03:30 +0200

    Update ssl_cert_issue.sh

--- ssl_cert_issue.sh
+++ ssl_cert_issue.sh
@@ -4,7 +4,7 @@
 echo "==================================================="
 echo " PAM RADIUS authentication setup for ocserv"
 echo " Introduction for CentOS7"
-echo " Author: SOROUSH"
+echo " Author: Hawax IT"
 echo " website:www.hawax.de"
 echo " Email: info@hawax.de"
 echo ""
